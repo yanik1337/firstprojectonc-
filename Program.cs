e631882577691c166594b7472336132971bd37a9
yanik1337 <144943233+yanik1337@users.noreply.github.com>
2024-12-04 18:34:40 +0300

    Programclass
    asd

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using PlayerClass;
+
+namespace Casino
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добро пожаловать в шедевроказино!");
+            Console.WriteLine("Выберите команду:");
+            Console.WriteLine("1. Ввести ник");
+            Console.WriteLine("0. Выйти.");
+
+            string input = "";
+            input = Console.ReadLine();
+
+            Player player = new Player();
+
+            while (input != "0") 
+            {
+                if (input == "1")
+                {
+                    player.PlayerNameInsert();
+                    Console.WriteLine();
+                    player.PlayerMoneyInsert();
+                    break;
+                }
+                
+            }
+        }
+    }
+}
